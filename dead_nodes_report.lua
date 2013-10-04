e6b3b5a122ae127e2ab624f3e94c2bca3ca099bb
Mike Carter <mike@buddasworld.co.uk>
2013-10-04 22:08:40 +0100

    Corrected indentation

--- dead_nodes_report.lua
+++ dead_nodes_report.lua
@@ -7,14 +7,14 @@ local i = 1;
 local TotalDevices = 40 + 1;
 
 while i < TotalDevices do 
+  local status = fibaro:getValue(i, 'dead'); 
 
-local status = fibaro:getValue(i, 'dead'); 
-
-if status == "1" then 
-  local desc = fibaro:getValue(i, "userDescription");
-  local name = fibaro:getName(i);
-  fibaro:debug("Some problems with device "..name.." ["..i.."] "..desc.." Please check!"); 
-  fibaro:call(2, "sendEmail", "Dead node report from Home Centre 2", "Some problems with device "..name.." ["..i.."] "..desc); 
-  else end 
+  if status == "1" then 
+    local desc = fibaro:getValue(i, "userDescription");
+    local name = fibaro:getName(i);
+    fibaro:debug("Some problems with device "..name.." ["..i.."] "..desc.." Please check!"); 
+    fibaro:call(2, "sendEmail", "Dead node report from Home Centre 2", "Some problems with device "..name.." ["..i.."] "..desc); 
+  else end
+  
   i = i + 1 
 end
