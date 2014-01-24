cc481bdffccb18990d7711b4f8ce1f035cd8f0d8
chris6783 <christophe.gtz@gmail.com>
2014-01-24 23:55:14 +0100

    avoid 1 mail per error
    Send only one mail with all dead nodes inside

--- dead_nodes_report.lua
+++ dead_nodes_report.lua
@@ -1,20 +1,27 @@
---[[ 
-%% properties 
-%% globals 
---]] 
+--[[
+%% properties
+%% globals
+--]]
 
 local i = 1;
 local TotalDevices = 40 + 1;
+local errors = 0
+local text = 'Some problems with device ;'
 
-while i < TotalDevices do 
-  local status = fibaro:getValue(i, 'dead'); 
-
-  if status == "1" then 
+while i < TotalDevices do
+  local status = fibaro:getValue(i, 'dead');
+  
+  if status == "1" then
+    errors = errors + 1
     local desc = fibaro:getValue(i, "userDescription");
     local name = fibaro:getName(i);
+    text = text ..  "\r "..name.."    ["..i.."] "..desc.." "
     fibaro:debug("Some problems with device "..name.." ["..i.."] "..desc.." Please check!"); 
-    fibaro:call(2, "sendEmail", "Dead node report from Home Centre 2", "Some problems with device "..name.." ["..i.."] "..desc); 
   else end
   
-  i = i + 1 
+  i = i + 1
 end
+text = text ..  "\r".. "Please check!"
+if errors >= 1 then
+ fibaro:call(2, "sendEmail", "Dead node report from Home Centre 2", text);
+end  
