55a88a10da7609379043eefbc6c000d0b21ce0fe
Hans Olav Hamran <hans@hamran.com>
2017-05-17 13:10:24 +0200

    Update wakeup_dead_nodes.lua
    The script failed when devices had been removed.

--- wakeup_dead_nodes.lua
+++ wakeup_dead_nodes.lua
@@ -1,48 +1,72 @@
---[[ 
-%% properties 
-%% autostart 
-%% globals 
---]] 
+--[[
+%% autostart
+%% properties
+%% events
+%% globals
+--]]
 
-local TotalDevices = 300 --max nr of devices 
+  -- added fix for removed devices
+
+
+local TotalDevices = 1000 --max nr of devices 
 
 while true do 
 
   local timeNow = os.date('*t') 
   local day = timeNow['day'] 
   local month = timeNow['month'] 
+  local d_status = "ok"
   
   local i = 1 
   local anydead = 0 
   while i < TotalDevices do 
     --check if any dead 
     local status = fibaro:getValue(i, 'dead'); 
+    if status ~= nil then
+      
+ 	if status >= "1" then
+        local d_status = "dead"
+        else
+        local d_status = "ok"
+    end
     local name = fibaro:getName(i); 
     local room = fibaro:getRoomNameByDeviceID(i); 
+    -- fibaro:debug(i.." "..name.." "..room.." "..d_status);
+  
     
     if status >= "1" then 
-      fibaro:debug(day.."/"..month..":"..i..' DEAD '..name..":"..room); 
+      local melding = (day.."/"..month..":"..i..' DEAD, trying to re-start '..name..":"..room);
+      fibaro:debug(melding); 
+      fibaro:call(448,'sendPush', melding);
       fibaro:wakeUpDeadDevice(i) 
-      fibaro:sleep(5000) --check again in 5 sec 
+      fibaro:sleep(50000) --check again in 50 sec 
       status = fibaro:getValue(i, 'dead'); 
       if status >= "1" then
-        anydead = 1; fibaro:debug('Really Dead') 
+        anydead = 1;
+        local melding = (day.."/"..month..":"..i..' DEAD, re-start failed '..name..":"..room);
+        fibaro:debug(melding)
+        fibaro:call(448,'sendPush', melding);
       else
-        fibaro:debug('Now OK '..name) 
-      end 
-    end 
-    i = i + 1 
+        local melding = (day.."/"..month..":"..i..' succeeded restart '..name..":"..room);
+        fibaro:debug(melding)
+        fibaro:call(448,'sendPush', melding);
+      end
+      else
+      end
+    end
+    i = i + 1
+
   end 
   
   if anydead == 0 then
     --fibaro:debug('Nobody is dead :-) ') 
   else 
-    fibaro:call(2, 'sendEmail', 'Somebody really DEAD', '..') 
     fibaro:debug('Somebody really DEAD') 
+    fibaro:call(2, "sendEmail", "Fibaro device is dead", melding); -- Mail 
   end 
   
   -- abort any unnecesary scenes started 
   if fibaro:countScenes() > 1 then fibaro:abort() end 
   
-  fibaro:sleep(15*60000) --repeat every 15 minutes 
-end
+  fibaro:sleep(360*60000) --repeat every 360 minutes 
+  end
